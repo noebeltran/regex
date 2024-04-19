5ca3c79491b5d7fea6d9a6b7ce2226d9c09ccfad
noe <nbeltran@iesrioarba.es>
2024-04-19 13:37:28 +0200

    main

--- regex.py
+++ regex.py
@@ -1,9 +1,9 @@
 import re
 
-regex = r"((M|m)+) [a-zA-Z0-1]{5,8} ((M|m)+)"
+regex = r"((M|m)+) [a-zA-Z0-1] {5,8} ((M|m)+)"
 findall = re.search(regex, "https://moure.dev")
 print(findall)
 
 regex = r"((M|m)+) [a-zA-Z0-1]((M|m)+)"
 findall = re.search(regex, "https://moure.dev")
-print(findall)
+print(findall).
