eba61f3e64f0f4efabf495e17d84fc02360b8360
noe <nbeltran@iesrioarba.es>
2024-04-19 14:20:15 +0200

    main

--- regex.py
+++ regex.py
@@ -1,9 +1,5 @@
 import re
 
-regex = r"((M|m)+) [a-zA-Z0-1] {5,8} ((M|m)+)"
-findall = re.search(regex, "https://moure.dev")
-print(findall)
-
-regex = r"((M|m)+) [a-zA-Z0-1]((M|m)+)"
-findall = re.search(regex, "https://moure.dev")
+regex = r"(www\.)\w{3,}\.n"
+findall = re.search(regex, "https://www.moure.dev")
 print(findall)
